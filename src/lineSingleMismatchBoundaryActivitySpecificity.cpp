65993d97d5bb55a3ade5ffe8811f12705c09ecba
Kee Myoung Nam <kmnam@g.harvard.edu>
2022-01-28 00:50:01 +0900

    Added option for specifying mismatch position

--- src/lineSingleMismatchBoundaryActivitySpecificity.cpp
+++ src/lineSingleMismatchBoundaryActivitySpecificity.cpp
@@ -20,12 +20,13 @@
  *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
  * 
  * **Last updated:**
- *     1/18/2022
+ *     1/27/2022
  */
 using namespace Eigen;
 using boost::multiprecision::number;
 using boost::multiprecision::mpfr_float_backend;
-using boost::multiprecision::log10; 
+using boost::multiprecision::log10;
+typedef number<mpfr_float_backend<100> > PreciseType; 
 
 const unsigned length = 20;
 
@@ -42,10 +43,11 @@ int coin_toss(boost::random::mt19937& rng)
 /**
  * Compute:
  * - cleavage activity on the perfect-match substrate and
- * - cleavage specificity with respect to the single-distal-mismatch substrate
- * for the line graph with the given set of parameter values. 
+ * - cleavage specificity with respect to the single-mismatch substrate
+ *   with the given mismatch position for the line graph with the given 
+ *   set of parameter values. 
  */
-template <typename T>
+template <typename T, int position>
 VectorXd computeCleavageStats(const Ref<const VectorXd>& params)
 {
     // Array of DNA/RNA match parameters
@@ -68,8 +70,9 @@ VectorXd computeCleavageStats(const Ref<const VectorXd>& params)
     T cleave_rate = 1; 
     T prob_perfect = model->getUpperExitProb(unbind_rate, cleave_rate);
 
-    // Introduce one distal mismatch and re-compute cleavage probability
-    model->setEdgeLabels(length - 1, mismatch_params);
+    // Introduce one mismatch at the specified position and re-compute
+    // cleavage probability
+    model->setEdgeLabels(position, mismatch_params); 
     T prob_mismatched = model->getUpperExitProb(unbind_rate, cleave_rate); 
 
     // Compile results and return 
@@ -109,26 +112,92 @@ int main(int argc, char** argv)
 
     // Boundary-finding algorithm settings
     double tol = 1e-6;
-    unsigned n_within = 10000;
+    unsigned n_within = 100;
     unsigned n_bound = 0;
     unsigned min_step_iter = 100;
-    unsigned max_step_iter = 500;
+    unsigned max_step_iter = 200;
     unsigned min_pull_iter = 10;
-    unsigned max_pull_iter = 100;
+    unsigned max_pull_iter = 50;
     unsigned max_edges = 500;
     bool verbose = true;
-    unsigned sqp_max_iter = 100;
+    unsigned sqp_max_iter = 50;
     double sqp_tol = 1e-3;
     bool sqp_verbose = false;
     std::stringstream ss;
-    ss << argv[3] << "-boundary";
+    ss << argv[3] << "-activity-spec" << argv[4] << "-boundary";
 
     // Run the boundary-finding algorithm
     BoundaryFinder finder(tol, rng, argv[1], argv[2]);
+    int position = std::stoi(argv[4]); 
     std::function<VectorXd(const Ref<const VectorXd>&, boost::random::mt19937&)> mutate = mutateByDelta<double>;
+    std::function<VectorXd(const Ref<const VectorXd>&)> func; 
+    switch (position)
+    {
+        case 0:
+            func = computeCleavageStats<PreciseType, 0>;
+            break;
+        case 1: 
+            func = computeCleavageStats<PreciseType, 1>;
+            break; 
+        case 2:
+            func = computeCleavageStats<PreciseType, 2>; 
+            break; 
+        case 3: 
+            func = computeCleavageStats<PreciseType, 3>; 
+            break; 
+        case 4: 
+            func = computeCleavageStats<PreciseType, 4>; 
+            break; 
+        case 5: 
+            func = computeCleavageStats<PreciseType, 5>; 
+            break; 
+        case 6: 
+            func = computeCleavageStats<PreciseType, 6>; 
+            break; 
+        case 7: 
+            func = computeCleavageStats<PreciseType, 7>; 
+            break; 
+        case 8: 
+            func = computeCleavageStats<PreciseType, 8>; 
+            break; 
+        case 9: 
+            func = computeCleavageStats<PreciseType, 9>; 
+            break; 
+        case 10: 
+            func = computeCleavageStats<PreciseType, 10>; 
+            break; 
+        case 11:
+            func = computeCleavageStats<PreciseType, 11>; 
+            break; 
+        case 12: 
+            func = computeCleavageStats<PreciseType, 12>; 
+            break; 
+        case 13: 
+            func = computeCleavageStats<PreciseType, 13>; 
+            break; 
+        case 14: 
+            func = computeCleavageStats<PreciseType, 14>; 
+            break; 
+        case 15:
+            func = computeCleavageStats<PreciseType, 15>; 
+            break; 
+        case 16: 
+            func = computeCleavageStats<PreciseType, 16>; 
+            break;
+        case 17: 
+            func = computeCleavageStats<PreciseType, 17>; 
+            break; 
+        case 18:
+            func = computeCleavageStats<PreciseType, 18>; 
+            break; 
+        case 19:
+            func = computeCleavageStats<PreciseType, 19>; 
+            break; 
+        default:
+            break; 
+    }
     finder.run(
-        computeCleavageStats<number<mpfr_float_backend<100> > >,
-        mutate, filter, n_within, n_bound, min_step_iter, max_step_iter,
+        func, mutate, filter, n_within, n_bound, min_step_iter, max_step_iter,
         min_pull_iter, max_pull_iter, max_edges, verbose, sqp_max_iter,
         sqp_tol, sqp_verbose, ss.str()
     );
@@ -136,7 +205,7 @@ int main(int argc, char** argv)
 
     // Write sampled parameter combinations to file
     std::ostringstream oss;
-    oss << argv[3] << "-boundary-params.tsv";
+    oss << argv[3] << "-activity-spec" << argv[4] << "-boundary-params.tsv";
     std::ofstream samplefile(oss.str());
     samplefile << std::setprecision(std::numeric_limits<double>::max_digits10);
     if (samplefile.is_open())
