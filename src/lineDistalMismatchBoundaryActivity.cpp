538dc98adf82b0b871a3a4f9e6fa74498b455a6f
Kee Myoung Nam <kmnam@g.harvard.edu>
2022-08-26 11:31:12 +0900

    Updated to reflect changes in boundary-finding algorithm

--- src/lineDistalMismatchBoundaryActivity.cpp
+++ src/lineDistalMismatchBoundaryActivity.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <tuple>
 #include <Eigen/Dense>
+#include <boost/container_hash/hash.hpp>
 #include <boost/math/constants/constants.hpp>
 #include <boost/multiprecision/mpfr.hpp>
 #include <boost/random.hpp>
@@ -20,7 +21,7 @@
  *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
  * 
  * **Last updated:**
- *     7/23/2022
+ *     8/26/2022
  */
 using namespace Eigen;
 using boost::multiprecision::number;
@@ -102,56 +103,247 @@ VectorXd computeCleavageStats(const Ref<const VectorXd>& input)
 }
 
 /**
+ * A version of `computeCleavageStats()` with the leftward terminal rate
+ * (the unbinding rate) set to unity, and the input vector specifying values
+ * for the other five parameters. 
+ */
+template <typename T, int position>
+VectorXd computeCleavageStatsUnbindingUnity(const Ref<const VectorXd>& input)
+{
+    // Array of DNA/RNA match parameters
+    std::pair<T, T> match;
+    match.first = static_cast<T>(std::pow(10.0, input(0)));
+    match.second = static_cast<T>(std::pow(10.0, input(1)));
+
+    // Array of DNA/RNA mismatch parameters
+    std::pair<T, T> mismatch;
+    mismatch.first = static_cast<T>(std::pow(10.0, input(2)));
+    mismatch.second = static_cast<T>(std::pow(10.0, input(3)));
+
+    // Populate each rung with DNA/RNA match parameters
+    LineGraph<T, T>* model = new LineGraph<T, T>(length);
+    for (int j = 0; j < length; ++j)
+        model->setEdgeLabels(j, match);
+    
+    // Compute cleavage probability and cleavage rate on the perfect-match
+    // substrate
+    T terminal_unbind_rate = 1;
+    T terminal_cleave_rate = static_cast<T>(std::pow(10.0, input(4))); 
+    T prob_perfect = model->getUpperExitProb(terminal_unbind_rate, terminal_cleave_rate);
+
+    // Introduce the specified number of distal mismatches and re-compute 
+    // cleavage probability
+    for (int j = 0; j < num_mismatches; ++j)
+        model->setEdgeLabels(length - 1 - j, mismatch); 
+    T prob_mismatched = model->getUpperExitProb(terminal_unbind_rate, terminal_cleave_rate); 
+
+    // Compile results and return 
+    VectorXd output(2);
+    output << static_cast<double>(prob_perfect),
+              static_cast<double>(log10(prob_perfect) - log10(prob_mismatched)); 
+
+    delete model;
+    return output;
+}
+
+/**
+ * A version of `computeCleavageStats()` with *both* terminal rates (cleavage
+ * and unbinding) set to unity, and the input vector specifying values for 
+ * the other four parameters.
+ */
+template <typename T, int position>
+VectorXd computeCleavageStatsBothTerminalUnity(const Ref<const VectorXd>& input)
+{
+    // Array of DNA/RNA match parameters
+    std::pair<T, T> match;
+    match.first = static_cast<T>(std::pow(10.0, input(0)));
+    match.second = static_cast<T>(std::pow(10.0, input(1)));
+
+    // Array of DNA/RNA mismatch parameters
+    std::pair<T, T> mismatch;
+    mismatch.first = static_cast<T>(std::pow(10.0, input(2)));
+    mismatch.second = static_cast<T>(std::pow(10.0, input(3)));
+
+    // Populate each rung with DNA/RNA match parameters
+    LineGraph<T, T>* model = new LineGraph<T, T>(length);
+    for (int j = 0; j < length; ++j)
+        model->setEdgeLabels(j, match);
+    
+    // Compute cleavage probability and cleavage rate on the perfect-match
+    // substrate
+    T terminal_unbind_rate = 1;
+    T terminal_cleave_rate = 1;
+    T prob_perfect = model->getUpperExitProb(terminal_unbind_rate, terminal_cleave_rate);
+
+    // Introduce the specified number of distal mismatches and re-compute 
+    // cleavage probability
+    for (int j = 0; j < num_mismatches; ++j)
+        model->setEdgeLabels(length - 1 - j, mismatch); 
+    T prob_mismatched = model->getUpperExitProb(terminal_unbind_rate, terminal_cleave_rate); 
+
+    // Compile results and return 
+    VectorXd output(2);
+    output << static_cast<double>(prob_perfect),
+              static_cast<double>(log10(prob_perfect) - log10(prob_mismatched)); 
+
+    delete model;
+    return output;
+}
+
+/**
  * Return the template specialization of `computeCleavageStats()` corresponding
- * to the given number of mismatches. 
+ * to the given mismatch position. 
  */ 
 template <typename T>
-std::function<VectorXd(const Ref<const VectorXd>&)> getCleavageFunc(int num_mismatches)
+std::function<VectorXd(const Ref<const VectorXd>&)> getCleavageFunc(int position, int dim)
 {
-    switch (num_mismatches)
+    if (dim == 6)
     {
-        case 1: 
-            return computeCleavageStats<PreciseType, 1>;
-        case 2:
-            return computeCleavageStats<PreciseType, 2>; 
-        case 3: 
-            return computeCleavageStats<PreciseType, 3>; 
-        case 4: 
-            return computeCleavageStats<PreciseType, 4>; 
-        case 5: 
-            return computeCleavageStats<PreciseType, 5>; 
-        case 6: 
-            return computeCleavageStats<PreciseType, 6>; 
-        case 7: 
-            return computeCleavageStats<PreciseType, 7>; 
-        case 8: 
-            return computeCleavageStats<PreciseType, 8>; 
-        case 9: 
-            return computeCleavageStats<PreciseType, 9>; 
-        case 10: 
-            return computeCleavageStats<PreciseType, 10>; 
-        case 11:
-            return computeCleavageStats<PreciseType, 11>; 
-        case 12: 
-            return computeCleavageStats<PreciseType, 12>; 
-        case 13: 
-            return computeCleavageStats<PreciseType, 13>; 
-        case 14: 
-            return computeCleavageStats<PreciseType, 14>; 
-        case 15:
-            return computeCleavageStats<PreciseType, 15>; 
-        case 16: 
-            return computeCleavageStats<PreciseType, 16>; 
-        case 17: 
-            return computeCleavageStats<PreciseType, 17>; 
-        case 18:
-            return computeCleavageStats<PreciseType, 18>; 
-        case 19:
-            return computeCleavageStats<PreciseType, 19>;
-        case 20:
-            return computeCleavageStats<PreciseType, 20>;  
-        default:
-            throw std::invalid_argument("Invalid number of mismatches"); 
+        switch (position)
+        {
+            case 1: 
+                return computeCleavageStats<PreciseType, 1>;
+            case 2:
+                return computeCleavageStats<PreciseType, 2>; 
+            case 3: 
+                return computeCleavageStats<PreciseType, 3>; 
+            case 4: 
+                return computeCleavageStats<PreciseType, 4>; 
+            case 5: 
+                return computeCleavageStats<PreciseType, 5>; 
+            case 6: 
+                return computeCleavageStats<PreciseType, 6>; 
+            case 7: 
+                return computeCleavageStats<PreciseType, 7>; 
+            case 8: 
+                return computeCleavageStats<PreciseType, 8>; 
+            case 9: 
+                return computeCleavageStats<PreciseType, 9>; 
+            case 10: 
+                return computeCleavageStats<PreciseType, 10>; 
+            case 11:
+                return computeCleavageStats<PreciseType, 11>; 
+            case 12: 
+                return computeCleavageStats<PreciseType, 12>; 
+            case 13: 
+                return computeCleavageStats<PreciseType, 13>; 
+            case 14: 
+                return computeCleavageStats<PreciseType, 14>; 
+            case 15:
+                return computeCleavageStats<PreciseType, 15>; 
+            case 16: 
+                return computeCleavageStats<PreciseType, 16>; 
+            case 17: 
+                return computeCleavageStats<PreciseType, 17>; 
+            case 18:
+                return computeCleavageStats<PreciseType, 18>; 
+            case 19:
+                return computeCleavageStats<PreciseType, 19>;
+            case 20:
+                return computeCleavageStats<PreciseType, 20>;
+            default:
+                throw std::invalid_argument("Invalid number of mismatches");
+        }
+    }
+    else if (dim == 5)
+    {
+        switch (position)
+        {
+            case 1: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 1>;
+            case 2:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 2>; 
+            case 3: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 3>; 
+            case 4: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 4>; 
+            case 5: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 5>; 
+            case 6: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 6>; 
+            case 7: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 7>; 
+            case 8: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 8>; 
+            case 9: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 9>; 
+            case 10: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 10>; 
+            case 11:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 11>; 
+            case 12: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 12>; 
+            case 13: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 13>; 
+            case 14: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 14>; 
+            case 15:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 15>; 
+            case 16: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 16>; 
+            case 17: 
+                return computeCleavageStatsUnbindingUnity<PreciseType, 17>; 
+            case 18:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 18>; 
+            case 19:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 19>;
+            case 20:
+                return computeCleavageStatsUnbindingUnity<PreciseType, 20>;
+            default:
+                throw std::invalid_argument("Invalid number of mismatches");
+        } 
+    }
+    else if (dim == 4)
+    {
+        switch (position)
+        {
+            case 1: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 1>;
+            case 2:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 2>; 
+            case 3: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 3>; 
+            case 4: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 4>; 
+            case 5: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 5>; 
+            case 6: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 6>; 
+            case 7: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 7>; 
+            case 8: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 8>; 
+            case 9: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 9>; 
+            case 10: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 10>; 
+            case 11:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 11>; 
+            case 12: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 12>; 
+            case 13: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 13>; 
+            case 14: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 14>; 
+            case 15:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 15>; 
+            case 16: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 16>; 
+            case 17: 
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 17>; 
+            case 18:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 18>; 
+            case 19:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 19>;
+            case 20:
+                return computeCleavageStatsBothTerminalUnity<PreciseType, 20>;
+            default:
+                throw std::invalid_argument("Invalid number of mismatches");
+        } 
+    }
+    else 
+    {
+        throw std::invalid_argument("Invalid parameter space dimension"); 
     }
 }
 
@@ -165,16 +357,19 @@ int main(int argc, char** argv)
         };
 
     // Boundary-finding algorithm settings
-    const int n_init = 20000; 
+    const int n_init = 40000; 
     const double tol = 1e-6;
-    const int min_step_iter = 10;
-    const int max_step_iter = 100;
-    const int min_pull_iter = 10;
-    const int max_pull_iter = 100;
+    const int min_step_iter = 100;
+    const int max_step_iter = 200;
+    const int min_pull_iter = 100;
+    const int max_pull_iter = 1000;
     const int sqp_max_iter = 100;
     const double sqp_tol = 1e-6;
-    const int max_edges = 500;
-    const int psi = 100;
+    const int max_edges = 2000;
+    int n_keep_interior = 10000; 
+    int n_keep_origbound = 10000;
+    int n_mutate_origbound = 400;
+    int n_pull_origbound = 400;
     const double tau = 0.5;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -185,16 +380,29 @@ int main(int argc, char** argv)
     const double c2 = 0.9;
     const bool verbose = true;
     const bool sqp_verbose = false;
+    const bool traversal_verbose = true;  
+    const bool write_pulled_points = true; 
     std::stringstream ss;
     ss << argv[3] << "-activity-mm" << argv[4] << "-boundary";
 
+    // Parse the given .poly file and store its contents as a string 
+    std::ifstream infile(argv[1]); 
+    std::stringstream ss2;
+    ss2 << infile.rdbuf(); 
+    infile.close();
+
     // Initialize the boundary-finding algorithm
-    const int num_mismatches = std::stoi(argv[4]); 
-    std::function<VectorXd(const Ref<const VectorXd>&)> func = getCleavageFunc<PreciseType>(num_mismatches);
+    const int num_mismatches = std::stoi(argv[4]);
+    std::size_t seed = 0; 
+    boost::hash_combine(seed, 1234567890); 
+    boost::hash_combine(seed, position); 
+    boost::hash_combine(seed, ss2.str()); 
+    rng.seed(seed); 
     BoundaryFinder* finder = new BoundaryFinder(
-        tol, rng, argv[1], argv[2],
-        Polytopes::InequalityType::GreaterThanOrEqualTo, func
+        tol, rng, argv[1], argv[2], Polytopes::InequalityType::GreaterThanOrEqualTo
     );
+    std::function<VectorXd(const Ref<const VectorXd>&)> func = getCleavageFunc<PreciseType>(num_mismatches, finder->getD());
+    finder->setFunc(func); 
     double mutate_delta = 0.1 * getMaxDist<double>(finder->getVertices());
 
     // Obtain the initial set of input points
@@ -203,9 +411,11 @@ int main(int argc, char** argv)
     // Run the boundary-finding algorithm  
     finder->run(
         mutate_delta, filter, init_input, min_step_iter, max_step_iter, min_pull_iter,
-        max_pull_iter, sqp_max_iter, sqp_tol, max_edges, psi, tau, delta, beta,
-        use_only_armijo, use_strong_wolfe, hessian_modify_max_iter, ss.str(),
-        RegularizationMethod::NOREG, 0, c1, c2, verbose, sqp_verbose 
+        max_pull_iter, sqp_max_iter, sqp_tol, max_edges, n_keep_interior,
+        n_keep_origbound, n_mutate_origbound, n_pull_origbound, tau, delta,
+        beta, use_only_armijo, use_strong_wolfe, hessian_modify_max_iter,
+        ss.str(), RegularizationMethod::NOREG, 0, c1, c2, verbose, sqp_verbose,
+        traversal_verbose, write_pulled_points 
     );
     
     delete finder;    
